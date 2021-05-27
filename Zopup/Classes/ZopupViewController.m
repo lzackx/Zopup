451d9a63c4a08435355bbce3a73bbed19a2002d0
lzackx <lzackx@lzackx.com>
2021-05-27 09:24:21 +0800

    remove: log

--- Zopup/Classes/ZopupViewController.m
+++ Zopup/Classes/ZopupViewController.m
@@ -18,41 +18,41 @@
 - (instancetype)init {
 	self = [super init];
 	if (self) {
-		NSLog(@"%s", __FUNCTION__);
+//		NSLog(@"%s", __FUNCTION__);
 	}
 	return self;
 }
 
 - (void)loadView {
 	[super loadView];
-	NSLog(@"%s", __FUNCTION__);
+//	NSLog(@"%s", __FUNCTION__);
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-	NSLog(@"%s", __FUNCTION__);
+//	NSLog(@"%s", __FUNCTION__);
 	[self.view setUserInteractionEnabled:YES];
 	self.view.backgroundColor = [UIColor clearColor];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
 	[super viewWillAppear:animated];
-	NSLog(@"%s", __FUNCTION__);
+//	NSLog(@"%s", __FUNCTION__);
 }
 
 - (void)viewDidAppear:(BOOL)animated {
 	[super viewDidAppear:animated];
-	NSLog(@"%s", __FUNCTION__);
+//	NSLog(@"%s", __FUNCTION__);
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
 	[super viewWillDisappear:animated];
-	NSLog(@"%s", __FUNCTION__);
+//	NSLog(@"%s", __FUNCTION__);
 }
 
 - (void)viewDidDisappear:(BOOL)animated {
 	[super viewDidDisappear:animated];
-	NSLog(@"%s", __FUNCTION__);
+//	NSLog(@"%s", __FUNCTION__);
 }
 
 @end
